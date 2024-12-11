21e06b1ab9e24e2afe6f3c253fbc8acaf163a619
anandak07 <anandkumark055@gmail.com>
2024-12-12 00:05:29 +0530

    notes upto linked list

--- completeDSAInJS8hrsVishalRajput/1.js
+++ completeDSAInJS8hrsVishalRajput/1.js
@@ -373,4 +373,725 @@ var reverseString = function(s) {
     // return revs; // Returns a new reversed array
 };
 
-console.log(reverseString(["H","e","l","l","o"]))
+console.log(reverseString(["H","e","l","l","o"]));
+
+// Q14. Longest Common Prefix https://leetcode.com/problems/longest-common-prefix/description/ 
+// strs = ["flower","flow","flight"] o/p fl
+var longestCommonPrefix = function(strs) {
+    strs.sort();
+    let a=strs[0];
+    let b=strs[strs.length-1];
+    let result="";
+    for(let i=0;i<a.length;i++){
+        if(a[i]==b[i]){
+            result+=a[i];
+        }else{
+            break;
+        }
+    }
+    return result;
+};
+
+
+let word1="abc";
+let word2="pqrel";
+
+// word1 = "abc", word2 = "pqr"
+// o/p "apbqcr"
+
+var mergeAlternately = function(word1, word2) {
+    let ans="";
+    let m=word1.length;
+    let n=word2.length;
+    for(let i=0;i<Math.max(m,n);i++){
+        if(i<m){
+            ans+=word1[i];
+        }
+        if(i<n){
+            ans+=word2[i];
+        }
+    }
+    return ans;
+};
+
+// 58. Length of Last Word https://leetcode.com/problems/length-of-last-word/description/
+let s = "   fly me   to   the moon  ";
+function lastWord(s){
+    return s.trim().split(" ").at(-1).length;
+}
+lastWord(s);
+
+// Recursion
+function fun(n){
+    // T.C O(n)
+    if(n==0)return;
+    console.log(`fun`,n);
+    fun(n-1);
+}
+fun(10);
+
+// Q2 factorial
+function factorial(n){
+    if(n===0){
+        return 1;
+    }
+    return n*factorial(n-1);
+}
+
+console.log(factorial(6));
+
+
+// Q3 sum of Array
+
+// function sumOfArray(array){
+//     if(array.length===0)return 0;
+//     // return array[array.length-1]+sumOfArray(array.slice(0,array.length-1));
+//     let lastNo=array.pop();
+//     return lastNo+sumOfArray(array)
+// }
+
+// method 3
+function sumOfArray(array,n){
+    if(n===0)return 0;
+    return arr[n-1]+sumOfArray(array,n-1);
+}
+
+console.log(sumOfArray([1,2,3,4,5],5))
+
+// find the product of the array
+function prodFun(array,n){
+    if(n==0) return 1;
+    return array[n-1]*prodFun(array,n-1)
+}
+
+console.log(prodFun([1,2,3,4,5],5))
+
+function fibonacciRecursion(n){
+    if(n<2) return n;
+    return fibonacciRecursion(n-1)+fibonacciRecursion(n-2);
+}
+
+console.log(fibonacciRecursion(6))
+// fibonacciRecursion(5) 0 1 1 2 3 5
+
+
+// linear search(2:49) 
+const larr=[1,2,3,4,5,9,2,4,5,0,-3];
+
+function linearSearch(arr,target){
+    for(let i=0;i<arr.length;i++){
+        if(arr[i]==target)return i;
+    }
+    return -1;
+}
+
+console.log(linearSearch(larr,9));
+console.log(larr.indexOf(9));
+console.log(larr.includes(9))
+console.log(larr.find((num)=>num>0));//num will be found
+console.log(larr.findIndex((num)=>num<0))//num's index will be found
+
+
+// 28. Find the Index of the First Occurrence in a String https://leetcode.com/problems/find-the-index-of-the-first-occurrence-in-a-string/description/
+var strStr = function(haystack, needle) {
+    // return haystack.indexOf(needle);
+    let m=haystack.length,n=needle.length;
+    for(let i=0;i<=m-n;i++){
+        let flag=true;
+        for(let j=0;j<n;j++){
+            if(haystack[i+j]!=needle[j]){
+                flag=false;
+                break;
+            }
+        }
+        if(flag){
+            return i;
+        }
+    }
+    return -1;
+};
+
+
+function binarySearch(arr,k){
+    let low=0,high=arr.length-1;
+    while(low<=high){
+        let mid=low+Math.floor((high-low)/2);
+        if(arr[mid]==k){
+            return mid;
+        }else if(k<arr[mid]){
+            high=mid-1;
+        }else if(k>arr[mid]){
+            low=mid+1;
+        }
+    }
+    return -1;
+}
+
+console.log(binarySearch([1,2,3,4,5,7,8,10,13,14,16,17],6));
+
+
+function binarySearchRecursion(arr,target,low,high){
+    if(low>high){
+        return -1;
+    }
+    let mid=low+Math.floor((high-low)/2)
+    if(arr[mid]==target){
+        return mid;
+    }else if(target<arr[mid]){
+        return binarySearchRecursion(arr,target,low,mid-1)
+    }else if(target>arr[mid]){
+        return binarySearchRecursion(arr,target,mid+1,high);
+    }
+}
+
+console.log(binarySearchRecursion([1,2,3,4,5,6,7,8,10,11,14],11,0,10),'binaryRecursion');
+
+
+
+var searchInsert = function(nums, target) {
+    let low=0;
+    let high=nums.length-1;
+    while(low<=high){
+        let mid=low+Math.floor((high-low)/2);
+        if(nums[mid]==target){
+            return mid;
+        }else if(target<nums[mid]){
+            high=mid-1;
+        }else if(target>nums[mid]){
+            low=mid+1;
+        }
+    }       
+        // CEIL(low),FLOOR(high)
+    return low;
+};
+console.log(searchInsert([1,2,4,5,6,8,9,15,16,19],10))
+
+
+// Objects in js
+const person={
+    name:"Anand",
+    "class":12,
+    role:"Backend devloper",
+    isStudent:false,
+    isWorking:false,
+    skill:["javaScript","Express.js","Node.js","MongoDB"],
+    codeFun:function(){
+        console.log('Normal function')
+    },
+    arrowFun:function(){
+        console.log('Arrow Funtion')
+    }
+}
+
+// How to access
+
+// Dot operator
+console.log(person.name);
+
+// []
+console.log(person["name"]);
+console.log(person.class);
+person.codeFun()
+person.arrowFun();
+
+// how to find the key in object
+console.log(person.hasOwnProperty("name"));
+person.name="AnandAk";
+console.log(person.name);
+
+// how to add new property
+person.location="Bangalore";
+console.log(person.location);
+
+// Shallow copy
+const person2=person;
+person2.isStudent=true;
+console.log(person.isStudent)
+console.log(person2.isStudent)
+
+// Deep copy
+const person3=Object.assign({},person);
+person3.isWorking=true;
+console.log(person3.isWorking);
+console.log(person.isWorking)
+
+// freeze: We can't able to add,delete,update
+// Object.freeze(person);
+person.name="Manju";
+console.log(person.name)
+console.log(Object.isFrozen(person));
+
+// freeze: We can't able to add,delete but able to update
+Object.seal(person);
+person.name="Hemanth";
+console.log(person.name);
+console.log(Object.isSealed(person));
+
+
+// keys,values,enteries
+console.log(Object.keys(person));
+console.log(Object.values(person));
+console.log(Object.entries(person))
+
+
+// loop through an object 
+// for ... in
+
+for(let key in person){
+    console.log(key+":"+person[key])
+}
+
+Object.keys(person).forEach((key)=>console.log(key))
+
+console.log(Object.is(person,person3))
+
+for(let key in person){
+    if(person[key]!==person3[key]){
+        console.log('person & person3 are not equal');
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Sorting in Js
+
+// Sort an Array
+const sarr=[-1,-5,-10,1000,6];
+console.log(sarr.sort())//[ -1, -10, -5, 1000, 6 ] it will convert arr elements as numbers sort it
+
+
+console.log(sarr.sort((a,b)=>a-b))//[ -10, -5, -1, 6, 1000 ]
+console.log(sarr.sort((a,b)=>b-a))//desending [ 1000, 6, -1, -5, -10 ]
+
+
+const sstr=["apple","fox","anand","cat","ball"];
+console.log(sstr.sort()) //works fine [ 'anand', 'apple', 'ball', 'cat', 'fox' ]
+
+const sstr1="Vishal";
+// console.log(sstr1.sort()) it's wrong sort is not a function
+console.log(sstr1.toLowerCase().split("").sort().join(""))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Sorting Algo
+
+// bubble sort
+const bubbleSort=(arr)=>{
+    for(let i=0;i<arr.length-1;i++){
+        for(let j=0;j<arr.length-1-i;j++){
+            if(arr[j]>arr[j+1]){
+                // let temp=arr[j];
+                // arr[j]=arr[j+1];
+                // arr[j+1]=temp
+                [arr[j],arr[j+1]]=[arr[j+1],arr[j]]
+            }
+        }
+    }
+    console.log(arr)
+}
+bubbleSort([6,2,7,8,9,1,3]);
+
+const selectionSort=(arr)=>{
+    let n=arr.length;
+    for(let i=0;i<n-1;i++){
+        let minIdx=i;
+        for(let j=i+1;j<n;j++){
+            if(arr[j]<arr[minIdx]){
+                minIdx=j;
+            }
+        }
+        if(minIdx!==i){
+            let temp=arr[i];
+            arr[i]=arr[minIdx];
+            arr[minIdx]=temp;
+        }
+    }
+    console.log(arr)
+}
+
+selectionSort([1,4,2,3,5,8,7,5])
+
+
+
+
+function insertionSort(arr){
+    for(let i=1;i<arr.length;i++){
+        let j=i-1;
+        let x=arr[i];
+        while(j>=0&&arr[j]>x){
+            arr[j+1]=arr[j];
+            j--;
+        }
+        arr[j+1]=x;
+    }
+    console.log(arr,"insertionSort")
+}
+
+insertionSort([1,2,5,3,4,9,8,7,6])
+
+const mergeSort=(arr)=>{
+    if(arr.length<2){
+        return arr;
+    }
+    let mid=Math.floor(arr.length/2);
+    let left=mergeSort(arr.slice(0,mid));
+    let right=mergeSort(arr.slice(mid));
+
+    return merge(left,right);
+}
+
+const merge=(left,right)=>{
+    let result=[];
+    let leftIndex=0,rightIndex=0;
+    while(leftIndex<left.length&&rightIndex<right.length){
+        if(left[leftIndex]<right[rightIndex]){
+            result.push(left[leftIndex]);
+            leftIndex++;
+        }else{
+            result.push(right[rightIndex]);
+            rightIndex++;
+        }
+    }
+    while(leftIndex<left.length){
+        result.push(left[leftIndex]);
+        leftIndex++;
+    }
+    while(rightIndex<right.length){
+        result.push(right[rightIndex]);
+        rightIndex++;
+    }
+    return result;
+}
+
+console.log(mergeSort([1,4,2,3,6,5,8,9,10]),'merge sort')
+
+
+const quickSort=(arr)=>{
+    if(arr.length<=1){
+        return arr;
+    }
+    let pivot=arr[arr.length-1];
+    let left=[];
+    let right=[];
+
+    for(let i=0;i<arr.length-1;i++){
+        if(arr[i]<pivot){
+            left.push(arr[i])
+        }else{
+            right.push(arr[i]);
+        }
+    }
+    return [...quickSort(left),pivot,...quickSort(right)]
+}
+
+console.log(quickSort([1,3,2,7,4,5,9,8]),'quicksort');
+
+
+// Map in JavaScript
+
+const map=new Map();
+// const object1=new Object();
+const object2={};
+
+obj["name"]="Vishal";
+obj["age"]=21;
+map.set("name","Anand");
+map.set(21,"age");
+map.set(true,"isEducated");
+
+console.log(map.delete("name"))
+console.log(map.has("name"));
+console.log(map.get(21))
+
+map.forEach((value,key)=>{
+    console.log(`${key}:${value}`)
+})
+
+// obj.forEach will not work
+Object.keys(obj).forEach((key,value)=>{
+    console.log(`${key}--${value}`)
+})
+
+                // return the keys 
+for(let value of map.keys()){
+    console.log(value)
+}
+
+map.clear();
+console.log(map)
+
+// Performance of Map & Object
+
+const obj2={};
+const array1=[1000000000].fill(0).map(()=>Math.random()*100000);
+
+console.time(`Obj time`)
+
+for(let i=0;i<array1.length;i++){
+    obj2[i]=array1[i];
+};
+console.timeEnd(`Obj time`)
+
+const map2=new Map();
+
+console.time(`Map time`)
+
+for(let i=0;i<array1.length;i++){
+    map2.set(array1[i])
+};
+console.timeEnd(`Map time`)
+
+
+
+// Weak Map in Js
+let obj3={"name":"Anand"}
+const map3=new Map();
+map3.set(obj3,'metadata');
+console.log(map3)
+obj3=null;
+console.log(map3);
+
+// Weak ex
+let obj4={"name":"manju"}
+let weakmap=new WeakMap();
+weakmap.set(obj4,'metadata2');
+console.log(weakmap);
+obj4=null;
+console.log(weakmap)
+
+// Weak map stores only functions & obj only
+// Map stores numbers,string all the data type
+
+
+// Set in Js(it returns only the uniq value)
+
+const set=new Set([1,2,3,1,2,3,4,8,6,7,5]);
+set.add(45);
+console.log(set)
+set.delete(1);
+console.log(set.has(2))
+console.log(set)
+
+for(let x of set.keys()){
+    console.log(x,'keys()')
+}
+
+for(let x of set.values()){
+    console.log(x,'values()')
+}
+
+set.forEach((value)=>console.log(value))
+console.log(set.size,'size')
+// 1.Map can store any data type but obj can store only "string" as a key
+// 2.Map is iterable
+// 3.Map is used to store key and value but obj methods and function can also be stored
+// 4.can find the size of the map but not the obj
+
+set.clear();
+console.log(set)
+
+// Q1: Union of 2 array(No duplicate values)
+const arra1=[1,2,3,4,5];
+const arra2=[1,2,4,6,7];
+
+console.log(new Set([...arra1,...arra2]));
+console.log([...new Set([...arra1,...arra2])]);
+
+
+// 451. Sort Characters By Frequency https://leetcode.com/problems/sort-characters-by-frequency/description/
+
+var frequencySort = function(s) {
+    const map=new Map();
+    for(let ch of s){
+      map.set(ch,(map.get(ch)||0)+1);
+    }
+  
+    const sortedArray=[...map.keys()].sort((a,b)=>map.get(b)-map.get(a));
+  
+    let resultString="";
+    for(let ch of sortedArray){
+      resultString+=ch.repeat(map.get(ch));
+    }
+  
+    return resultString;
+  };
+
+// 349. Intersection of Two Arrays https://leetcode.com/problems/intersection-of-two-arrays/
+
+var intersection = function(nums1, nums2) {
+    //   let result=[];
+    //   for(let i=0;i<nums1.length;i++){
+    //     for(let j=0;j<nums2.length;j++){
+    //         if(nums1[i]==nums2[j]&&!result.find((x)=>x==nums1[i])){
+    //             result.push(nums1[i]);
+    //             break;
+    //         }
+    //     }
+    
+        let result=new Set();
+        let nums2Set=new Set(nums2);
+        for(let i=0;i<nums1.length;i++){
+            if(nums2Set.has(nums1[i])&& !result.has(nums1[i])){
+                result.add(nums1[i]);
+            }
+        }
+        return [...result];
+    };
+
+
+
+
+    // Linked List 
+
+
+class Node{
+    constructor(data){
+        this.data=data;
+        this.next=null;
+    }
+}
+
+class LinkedList{
+    constructor(){
+        this.head=null;
+        this.size=0;
+    }
+
+    insertAtHead(data){
+        const newNode=new Node(data);
+        newNode.next=this.head;
+        this.head=newNode
+    }
+
+    print(){
+        let result="";
+        let temp=this.head;
+        while(temp){// or temp!=null 
+            result+=`${temp.data}->`;
+            temp=temp.next;
+        }
+        return result
+    }
+}
+
+let list=new LinkedList();
+list.insertAtHead(43)
+list.insertAtHead(34)
+list.insertAtHead(13)
+console.log(list.print())
+console.log(list)
+
+
+// 876. Middle of the Linked List https://leetcode.com/problems/middle-of-the-linked-list/
+
+var middleNode = function(head) {
+    let slow=head,fast=head;
+    while(fast&&fast.next){
+        fast=fast.next.next;
+        slow=slow.next;
+    }
+    return slow;
+};
+
+// 141. Linked List Cycle https://leetcode.com/problems/linked-list-cycle/description/
+
+var hasCycle = function(head) {
+    let slow=head,fast=head;
+    while(fast&&fast.next){
+        fast=fast.next.next;
+        slow=slow.next;
+
+        if(slow==fast){
+            return true;
+        }
+    }
+    return false;
+};
+
+
+// 206. Reverse Linked List https://leetcode.com/problems/reverse-linked-list/description/
+
+var reverseList = function(head) {
+    let prev=null,curr=head,temp;
+    while(curr){
+        temp=curr.next;
+        curr.next=prev;
+        prev=curr;
+        curr=temp;
+    }
+    return prev;
+};
